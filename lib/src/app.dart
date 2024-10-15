d4ec9be2929cc940b1b8081ce74f4533baaee6b9
Akash M <akashm006@outlook.com>
2024-10-15 21:02:06 +0530

    Added orientation lock

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,18 @@
 import 'package:fit_chrono/src/core/constants/settings.dart';
 import 'package:fit_chrono/src/routing/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp.router(
       title: appName,
       routerConfig: router,
