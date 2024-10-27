731d8d93516bf91e8194b52977ee4b58c307c0f1
Akash M <akashm006@outlook.com>
2024-10-27 14:01:16 +0530

    Implemented the domain layer

--- lib/src/features/muscle_maps/domain/usecase/delete_muscle_map.usecase.dart
+++ lib/src/features/muscle_maps/domain/usecase/delete_muscle_map.usecase.dart
@@ -1,10 +1,14 @@
 import 'package:fit_chrono/src/core/usecase/usecase.dart';
 import 'package:fit_chrono/src/core/utils/data_state.util.dart';
+import 'package:fit_chrono/src/features/muscle_maps/domain/repository/muscle_map.repository.dart';
 
 class DeleteMuscleMapUsecase extends Usecase<Future<DataState<void>>, int> {
+  final MuscleMapRepository _muscleMapRepository;
+
+  DeleteMuscleMapUsecase(this._muscleMapRepository);
+
   @override
   Future<DataState<void>> call({required int params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return _muscleMapRepository.deleteMuscleMap(params);
   }
 }
