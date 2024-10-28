0eaa142fa0cd5621708e5413bcb5edb8a76c7911
Akash M <akashm006@outlook.com>
2024-10-28 20:06:52 +0530

    Implemented domain for watching workouts

---
+++ lib/src/features/workout/domain/usecase/watch_workouts.usecase.dart
@@ -0,0 +1,14 @@
+import 'package:fit_chrono/src/core/usecase/usecase.dart';
+import 'package:fit_chrono/src/features/workout/domain/entity/workout.entity.dart';
+import 'package:fit_chrono/src/features/workout/domain/repository/workout.repository.dart';
+
+class WatchWorkoutsUsecase extends Usecase<Stream<List<WorkoutEntity>>, void> {
+  final WorkoutRepository _workoutRepository;
+
+  WatchWorkoutsUsecase(this._workoutRepository);
+
+  @override
+  Stream<List<WorkoutEntity>> call({required void params}) {
+    return _workoutRepository.watchWorkouts();
+  }
+}
