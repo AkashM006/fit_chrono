0ebfe29ca136412f59a6f9206055dadcd459c2ad
Akash M <akashm006@outlook.com>
2024-10-28 14:43:55 +0530

    Setup for schema of workout dont

---
+++ lib/src/features/workout/data/data_sources/local/schema/workout.schema.dart
@@ -0,0 +1,8 @@
+import 'package:drift/drift.dart';
+
+class Workouts extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text()();
+  IntColumn get repitition => integer()();
+  TextColumn get repititionType => text()();
+}
