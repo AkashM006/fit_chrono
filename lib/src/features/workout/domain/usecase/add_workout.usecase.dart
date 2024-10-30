cc82d31bcdd843fd1641c98eb2f9e2fee1fc4230
Akash M <akashm006@outlook.com>
2024-10-30 14:59:36 +0530

    Completed domain layer of adding workout

--- lib/src/features/workout/domain/usecase/add_workout.usecase.dart
+++ lib/src/features/workout/domain/usecase/add_workout.usecase.dart
@@ -1,12 +1,16 @@
 import 'package:fit_chrono/src/core/usecase/usecase.dart';
 import 'package:fit_chrono/src/core/utils/data_state.util.dart';
 import 'package:fit_chrono/src/features/workout/domain/entity/workout.entity.dart';
+import 'package:fit_chrono/src/features/workout/domain/repository/workout.repository.dart';
 
 class AddWorkoutUsecase
     extends Usecase<Future<DataState<void>>, WorkoutEntity> {
+  final WorkoutRepository _workoutRepository;
+
+  AddWorkoutUsecase(this._workoutRepository);
+
   @override
   Future<DataState<void>> call({required WorkoutEntity params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return _workoutRepository.addWorkout(params);
   }
 }
