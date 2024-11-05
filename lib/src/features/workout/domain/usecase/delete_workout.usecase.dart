8364884eb24525c7df776b9d5febc5447c0eea10
Akash M <akashm006@outlook.com>
2024-11-05 11:11:35 +0530

    Implemented domain layer

--- lib/src/features/workout/domain/usecase/delete_workout.usecase.dart
+++ lib/src/features/workout/domain/usecase/delete_workout.usecase.dart
@@ -1,10 +1,13 @@
 import 'package:fit_chrono/src/core/usecase/usecase.dart';
 import 'package:fit_chrono/src/core/utils/data_state.util.dart';
+import 'package:fit_chrono/src/features/workout/domain/repository/workout.repository.dart';
 
 class DeleteWorkoutUsecase extends Usecase<Future<DataState<void>>, int> {
+  final WorkoutRepository _workoutRepository;
+
+  DeleteWorkoutUsecase(this._workoutRepository);
+
   @override
-  Future<DataState<void>> call({required int params}) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<DataState<void>> call({required int params}) =>
+      _workoutRepository.deleteWorkout(params);
 }
