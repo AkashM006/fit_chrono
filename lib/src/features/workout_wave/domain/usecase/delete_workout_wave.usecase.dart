170250386e7f6d0ead8302fd0b95c08b48afddcf
Akash M <akashm006@outlook.com>
2024-11-22 12:03:45 +0530

    Workout wave delete domain completed

--- lib/src/features/workout_wave/domain/usecase/delete_workout_wave.usecase.dart
+++ lib/src/features/workout_wave/domain/usecase/delete_workout_wave.usecase.dart
@@ -1,10 +1,13 @@
 import 'package:fit_chrono/src/core/usecase/usecase.dart';
 import 'package:fit_chrono/src/core/utils/data_state.util.dart';
+import 'package:fit_chrono/src/features/workout_wave/domain/repository/workout_wave.repository.dart';
 
 class DeleteWorkoutWaveUsecase extends Usecase<Future<DataState<void>>, int> {
+  final WorkoutWaveRepostiory _workoutWaveRepostiory;
+
+  const DeleteWorkoutWaveUsecase(this._workoutWaveRepostiory);
+
   @override
-  Future<DataState<void>> call({required int params}) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<DataState<void>> call({required int params}) =>
+      _workoutWaveRepostiory.deleteWorkoutWave(params);
 }
