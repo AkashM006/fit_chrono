539db6f5d9c90acfefc591bc56f96415d1c2bd99
Akash M <akashm006@outlook.com>
2024-11-24 11:51:55 +0530

    Domain layer complete for workout search

--- lib/src/features/workout/domain/usecase/watch_workouts_search.usecase.dart
+++ lib/src/features/workout/domain/usecase/watch_workouts_search.usecase.dart
@@ -1,11 +1,14 @@
 import 'package:fit_chrono/src/core/usecase/usecase.dart';
 import 'package:fit_chrono/src/features/workout/domain/entity/workout.entity.dart';
+import 'package:fit_chrono/src/features/workout/domain/repository/workout.repository.dart';
 
 class WatchWorkoutsSearchUsecase
     extends Usecase<Stream<List<WorkoutEntity>>, String> {
+  final WorkoutRepository _workoutRepository;
+
+  const WatchWorkoutsSearchUsecase(this._workoutRepository);
+
   @override
-  Stream<List<WorkoutEntity>> call({required String params}) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Stream<List<WorkoutEntity>> call({required String params}) =>
+      _workoutRepository.watchWorkoutsSearch(params);
 }
